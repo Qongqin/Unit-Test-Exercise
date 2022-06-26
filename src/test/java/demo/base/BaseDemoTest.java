70a23bc77f4b815ecf4ada23b8a6e6c4f9209004
Qongqin <1452549329@qq.com>
2022-06-26 23:47:22 +0800

    Add 2 test cases

--- src/test/java/demo/base/BaseDemoTest.java
+++ src/test/java/demo/base/BaseDemoTest.java
@@ -14,4 +14,22 @@ public class BaseDemoTest {
 
         assertEquals(expected, baseDemo.print());
     }
+
+    @Test
+    public void should_return_5() {
+        BaseDemo baseDemo = new BaseDemo();
+
+        int expected = 5;
+
+        assertEquals(expected, baseDemo.sum(3, 2));
+    }
+
+    @Test
+    public void should_return_20() {
+        BaseDemo baseDemo = new BaseDemo();
+
+        int expected = 5;
+
+        assertEquals(expected, baseDemo.sum(15, 5));
+    }
 }
